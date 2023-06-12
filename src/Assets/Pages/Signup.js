a763b34a7916699acab3855e974c0f2c2b6a6aa3
ALi butt <alibutt8821@gmail.com>
2023-06-12 16:02:52 +0500

    add continue with google

--- src/Assets/Pages/Signup.js
+++ src/Assets/Pages/Signup.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button, Image } from "react-bootstrap";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
+import {images} from "../../Images"
 import Signupform from "../Components/Signupform";
 const Signup = () => {
   return (
@@ -21,6 +22,21 @@ const Signup = () => {
             </Col>
             <Col md={3}></Col>
           </Row>
+          <Row>
+                        <Col md={3}></Col>
+                        <Col md={6} className='paddingformregistration'>
+                            <div className='makehrandorinrow mt-5 mb-5'>
+                                <hr className='registrationpagehr'></hr>
+                                <p className='registrationor'>Or</p>
+                                <hr className='registrationpagehr'></hr>
+                            </div>
+                            <div className='makebuttonsincenterregistersociallogin'>
+                                <Button className='registrationthroughgoogleaddmorepadding'><Image className='setregistersocial' src={images.registergoogle} />Continue with google</Button>
+                                <Button className='registrationthroughgoogle mt-4'><Image className='setregistersocial' src={images.registerfacebook} />Continue with facebook</Button>
+                            </div>
+                        </Col>
+                        <Col md={3}></Col>
+                    </Row>
         </Container>
       </div>
       <Footer />
